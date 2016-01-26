e2d9896e77c59d87c64e0ed762daf7b8ba6b6062
David Abrahams <david.abrahams@students.olin.edu>
2016-01-26 02:29:16 -0500

    did node tutorial

---
+++ lessons/02-javascript/hw/index.js
@@ -0,0 +1,11 @@
+var server = require('./server');
+var router = require('./router');
+var requestHanders = require('./requestHandlers');
+
+
+var handle = {};
+handle["/"] = requestHanders.start;
+handle["/start"] = requestHanders.start;
+handle["/upload"] = requestHanders.upload;
+
+server.start(router.route, handle);
