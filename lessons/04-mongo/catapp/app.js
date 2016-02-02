7f1aa0c2a68b667289d8494e17585f01ef151c89
David Abrahams <david.abrahams@students.olin.edu>
2016-02-02 15:37:04 -0500

    fixed db connection message

--- lessons/04-mongo/catapp/app.js
+++ lessons/04-mongo/catapp/app.js
@@ -13,13 +13,11 @@ var app = express();
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/cats');
 
-// THIS CODE DOESN'T WORK EVEN THOUGH IT'S STRAIGHT OFF THE GETTING STARTED
-// GUIDE. IT SAYS connectoin IS UNDEFINED.
-// var db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function() {
-//   connection.log('db connection successful!');
-// });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('db connection successful!');
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
