cb47e599a1b9b4782144ea1aa493557819548371
David Abrahams <david.abrahams@students.olin.edu>
2016-02-06 00:30:05 -0500

    got editting working

--- lessons/05-client-jquery-ajax/burgerapp/public/javascripts/ingredients.js
+++ lessons/05-client-jquery-ajax/burgerapp/public/javascripts/ingredients.js
@@ -1,9 +1,17 @@
-var $form = $("#add_form");
+var $add_form = $("#add_form");
+var $edit_form = $("#edit_form");
 var $table = $("#i_table");
+
+var updateButtons = function()
+{
+  $(".out_of_stock").click(out_of_stock_func);
+  $(".edit_button").click(edit_button_click);
+}
 
 var onPostSuccess = function(data, status) {
   var table = document.getElementById("i_table");
   var row = table.insertRow(-1);
+  row.setAttribute("id", data._id);
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
   var cell3 = row.insertCell(2);
@@ -11,7 +19,8 @@ var onPostSuccess = function(data, status) {
   cell1.innerHTML = data.name;
   cell2.innerHTML = data.price;
   cell3.innerHTML = '<button type="button" class="out_of_stock">Out of Stock</button>';
-  cell4.innerHTML = '<button type="button">Edit</button>';
+  cell4.innerHTML = '<button type="button" class="edit_button">Edit</button>';
+  updateButtons();
 };
 
 var onDeleteSuccess = function(data, status) {
@@ -19,12 +28,30 @@ var onDeleteSuccess = function(data, status) {
 }
 
 var onError = function(data, status) {
+  window.alert("An error occurred! :(");
 };
 
-$form.submit(function(event) {
+var out_of_stock_func = function () {
+  var id = $(this).closest("tr").attr('id');
+  $.post("ingredients/delete", id)
+  .done(onDeleteSuccess)
+  .error(onError);
+};
+
+var edit_button_click = function () {
+  $add_form.hide();
+  $edit_form.show();
+  var row = $(this).closest("tr");
+  var id = row.attr('id');
+  $edit_form.find("[name='name']").val(row.find('td').eq(0).text())
+  $edit_form.find("[name='price']").val(row.find('td').eq(1).text())
+  $("#selected").val(id);
+};
+
+$add_form.submit(function(event) {
   event.preventDefault();
-  var name = $form.find("[name='name']").val();
-  var price = $form.find("[name='price']").val();
+  var name = $add_form.find("[name='name']").val();
+  var price = $add_form.find("[name='price']").val();
   $.post("ingredients", {
     name: name,
     price: price
@@ -33,9 +60,28 @@ $form.submit(function(event) {
   .error(onError);
 });
 
-$(".out_of_stock").click(function () {
-  var row = $(this).closest("tr");
-  $.post("ingredients/delete", {id: row.attr('id')})
-  .done(onDeleteSuccess)
+$edit_form.submit(function(event) {
+  event.preventDefault();
+  var name = $edit_form.find("[name='name']").val();
+  var price = $edit_form.find("[name='price']").val();
+  var id = $('#selected').val();
+  $.post("ingredients/edit", {
+    name: name,
+    price: price,
+    id: id
+  })
+  .done(function(data, status) {
+    row = $('#' + id);
+    row.find('td').eq(0).text(name);
+    row.find('td').eq(1).text(price);
+  })
   .error(onError);
 });
+
+$edit_form.on('reset', function(e)
+{
+  $add_form.show();
+  $edit_form.hide();
+});
+
+updateButtons();
--- lessons/05-client-jquery-ajax/burgerapp/routes/ingredientRoutes.js
+++ lessons/05-client-jquery-ajax/burgerapp/routes/ingredientRoutes.js
@@ -13,26 +13,44 @@ routes.ingredientRoutesGET = function(req, res) {
 };
 
 routes.ingredientRoutesPOST = function(req, res) {
-	var params = req.body;
-	var ing = new Ingredient({name: params.name, price: params.price});
-	ing.save(function (err) {
-    	if (err) {
-    		console.log("Problem saving", err);
-    		res.status(500).send("Error!");
-    	}
-    	else {res.send(ing)}
-  	});
+    var params = req.body;
+    var ing = new Ingredient({name: params.name, price: params.price});
+    ing.save(function (err) {
+        if (err) {
+            console.log("Problem saving", err);
+            res.status(500).send("Error!");
+        }
+        else {res.send(ing)}
+    });
 };
 
 routes.ingredientRoutesDELETE = function(req, res) {
-	var params = req.body;
-	Ingredient.find({_id: params.id}).remove(function(err, result) {
+    var id = req.body;
+    Ingredient.find({_id: id}).remove(function(err, result) {
       if (err) {
-      	console.log("Problem deleting", err);
-      	res.status(500).send("Error!");
+        console.log("Problem deleting", err);
+        res.status(500).send("Error!");
       }
-      else {res.send(params.id)}
+      else {res.send(params.id);}
     });
 };
+
+
+routes.ingredientRoutesEDIT = function(req, res) {
+    var params = req.body;
+
+    Ingredient.update({ _id: params.id },
+                      { $set: { name: params.name, price: params.price }},
+                      function (err, raw) {
+                        if (err) {
+                            console.log("Problem editing", err);
+                            res.status(500).send("Error!");
+                        }
+                        else {
+                            res.send();
+                        }
+                      });
+};
+
 
 module.exports = routes;
